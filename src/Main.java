8faa1e0667374ffcd51ce303178e5804e09c5520
Dinuka Samarasinghe <dinukad735@gmail.com>
2025-11-05 12:30:27 +0530

    check -1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String args[]){
         System.out.println("Welcome to Care Management System");
+        System.out.println("Git commit check.....");
     }
 }
