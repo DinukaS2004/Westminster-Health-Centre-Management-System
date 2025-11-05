13ec4569ff4fe8f48d48b4d8ada24b9ada9a04e0
Dinuka Samarasinghe <dinukad735@gmail.com>
2025-11-05 16:35:32 +0530

    implemented Receptionist class

--- src/com/westminster/healthcare/model/Receptionist.java
+++ src/com/westminster/healthcare/model/Receptionist.java
@@ -1,4 +1,39 @@
 package com.westminster.healthcare.model;
 
-public class Receptionist {
+public class Receptionist extends StaffMember{
+
+    private String deskNumber;
+    private String hoursPerWeek;
+
+    public Receptionist(int id, String name, String surName, String dob, String contactNo) {
+
+        super(id,name,surName,dob,contactNo);
+        this.deskNumber=deskNumber;
+        this.hoursPerWeek=hoursPerWeek;
+
+    }
+    public  String getDeskNumber() {
+        return deskNumber;
+    }
+    public String getHoursPerWeek() {
+        return hoursPerWeek;
+    }
+
+    public void setHoursPerWeek(String hoursPerWeek) {
+        this.hoursPerWeek = hoursPerWeek;
+    }
+    public void setDeskNumber(String deskNumber) {
+        this.deskNumber = deskNumber;
+    }
+
+    @Override
+    public String getRole() {
+        return "Receptionist";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format(" | Desk: %s | Hours/Week: %d",
+                deskNumber, hoursPerWeek);
+    }
 }
